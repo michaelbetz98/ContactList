d8afaae696b1ef08e503e49f9f620794928bad9f
michaelbetz <michael.schabetz@stud.tecnicosuperiorekennedy.it>
2024-04-10 16:27:03 +0200

    sada test 1

---
+++ ContactList/List.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    
+    public static class List
+    {
+        public static List<string> names = new List<string>();
+
+
+        public static void AddContact(string name)
+        {
+            bool alredyThere = false;
+            foreach(string checkname in names)
+            {
+                if (checkname == name)
+                {
+                    alredyThere = true;
+                }
+            }
+            if (alredyThere == false)
+            {
+                names.Add(name);
+            }
+        }
+
+        public static void PrintList() 
+        {
+            foreach (string name in names) 
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static string SearchContact(string name)
+        {
+            bool alredyThere = false;
+            foreach (string checkname in names)
+            {
+                if (checkname == name)
+                {
+                    return $"Trovato {name}";
+                }
+            }
+            if (alredyThere == false)
+            {
+                return $"Non trovato {name}";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public static void RemoveContact(string name)
+        {
+            bool alredyThere = false;
+            foreach (string checkname in names)
+            {
+                if (checkname == name)
+                {
+                    names.Remove(name);
+                    return;
+                }
+            }
+        }
+
+    }
+}
--- ContactList/Program.cs
+++ ContactList/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            do
+            {
+                Console.Clear();
+                List.PrintList();
+                string line = Console.ReadLine();
+                string[] command = line.Split(' ');
+                switch (command[0])
+                {
+                    case "add":
+                        List.AddContact(command[1]);
+                        break;
+                    case "search":
+                        List.SearchContact(command[1]);
+                        break;
+                    case "remove":
+                        List.RemoveContact(command[1]);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            while (true);
+            
         }
     }
 }
